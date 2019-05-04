f6c0f6a032c77994fd2f99f58d7ea4ca90d35abc
Sergey Yrkovskiy <reliablesy@gmail.com>
2019-05-05 00:22:14 +0300

    global

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
+function procent_in_top() {
+    var old_price, new_price, sell_in_procent;
+    old_price = document.getElementById('old').value;
+    old_price = parseInt(old_price);
+    new_price = document.getElementById('new').value;
+    new_price = parseInt(new_price);
+    sell_in_procent = (new_price * 100)/old_price;
+    document.getElementById('out').innerHTML = Math.floor(sell_in_procent);
+}
 
 
